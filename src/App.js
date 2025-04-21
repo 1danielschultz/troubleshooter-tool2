94a6eef166dd974af4d7cbe1f1bd9b826832cfce
1danielschultz <1danielschultz@gmail.com>
2025-04-21 14:40:30 -0600

    Switch to HashRouter for GitHub Pages compatibility

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { HashRouter as Router, Routes, Route } from 'react-router-dom';
 import { TroubleshooterProvider } from './context/TroubleshooterContext';
 import GlobalStyles from './styles/GlobalStyles';
 import TroubleshooterPage from './pages/TroubleshooterPage';
